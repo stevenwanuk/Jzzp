e26a9553cd6fb7b81afae1f323086e655e95743f
stevenwanuk <stevenwanuk@gmail.com>
2016-04-19 13:25:34 +0800

    Fixed gmap issue

---
+++ TP/WindowForm/GMapWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TP.WindowForm
+{
+    /// <summary>
+    /// Interaction logic for GMapxaml.xaml
+    /// </summary>
+    public partial class GMapWindow : Window
+    {
+        public GMapWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
