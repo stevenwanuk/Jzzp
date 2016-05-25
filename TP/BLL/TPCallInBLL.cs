be315cbb5e28dc2327d401d6eb59498c96b9243e
KWan <KWan@BMJ.com>
2016-05-25 22:55:50 +0100

    fix bugs

--- TP/BLL/TPCallInBLL.cs
+++ TP/BLL/TPCallInBLL.cs
@@ -33,6 +33,9 @@ namespace TP.BLL
                         {
                             billRef.UserId_FK = query.UserId_FK;
                             billRef.AddressId_FK = query.AddressId_FK;
+                            billRef.DeliverFeeOrigin = query.DeliverFeeOrigin;
+                            billRef.DeliverFee = query.DeliverFeeOrigin;
+                            billRef.DeliverMiles = query.DeliverMiles;
                         }
                         
                     }
